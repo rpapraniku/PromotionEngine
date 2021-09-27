83a0e4e9ac0113b0602396bbd9411efb82b31c4b
rpapraniku <Refik.Papraniku@endava.com>
2021-09-27 15:20:48 +0200

    added application infrastructure

---
+++ PromotionEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PromotionEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PromotionEngineTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PromotionEngineTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
