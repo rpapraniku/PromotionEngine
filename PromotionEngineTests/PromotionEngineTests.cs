a531025dac1f60a0396358ad48f6bfa3275eca94
rpapraniku <Refik.Papraniku@endava.com>
2021-09-28 14:18:29 +0200

    Updated test accordingly

--- PromotionEngineTests/PromotionEngineTests.cs
+++ PromotionEngineTests/PromotionEngineTests.cs
@@ -18,16 +18,16 @@ namespace PromotionEngineTests
             {
                 Items = new List<OrderItem>
                 {
-                    new OrderItem { Quantity = 3, SKU = "A", Price = 50 },
-                    new OrderItem { Quantity = 2, SKU = "B", Price = 30 },
+                    new OrderItem { Quantity = 1, SKU = "A", Price = 50 },
+                    new OrderItem { Quantity = 1, SKU = "B", Price = 30 },
                     new OrderItem { Quantity = 1, SKU = "C", Price = 20 }
                 }
             };
 
             var promotions = new List<Promotion> {
                 new Promotion { BundleType = BundleType.Multiple, SKU = "A", Quantity = 3, DiscountType = DiscountType.FixedPrice, FixedPriceDiscount = 130},
-                new Promotion { BundleType = BundleType.Multiple, SKU = "B", Quantity = 2 , DiscountType = DiscountType.Percentage, PercentageDiscount = 10},
-                new Promotion { BundleType = BundleType.Combination, SKUs = new List<string> { "C", "D" }, DiscountType = DiscountType.Percentage, FixedPriceDiscount = 30 }
+                new Promotion { BundleType = BundleType.Multiple, SKU = "B", Quantity = 2 , DiscountType = DiscountType.FixedPrice, FixedPriceDiscount = 45},
+                new Promotion { BundleType = BundleType.Combination, SKUs = new List<string> { "C", "D" }, DiscountType = DiscountType.FixedPrice, FixedPriceDiscount = 30 }
             };
 
             //Act
@@ -38,10 +38,10 @@ namespace PromotionEngineTests
                 orderResults.MultipleBundleItems.Sum(x => x.Amount) +
                 orderResults.CombinationBundleItems.Sum(x => x.Amount);
 
-            Assert.Equal(1, orderResults.SingleItems.Sum(x => x.ItemCount));
-            Assert.Equal(184, orderResults.MultipleBundleItems.Sum(x => x.Amount));
+            Assert.Equal(3, orderResults.SingleItems.Sum(x => x.ItemCount));
+            Assert.Equal(0, orderResults.MultipleBundleItems.Sum(x => x.Amount));
             Assert.Equal(0, orderResults.CombinationBundleItems.Sum(x => x.Amount));
-            Assert.Equal(204, totalSum);
+            Assert.Equal(100, totalSum);
         }
 
         [Fact]
@@ -52,18 +52,16 @@ namespace PromotionEngineTests
             {
                 Items = new List<OrderItem>
                 {
-                    new OrderItem { Quantity = 5, SKU = "A", Price = 50 }, //MULTIPLE 130 
-                    new OrderItem { Quantity = 7, SKU = "B", Price = 30 }, //MULTIPLE 48 + 48 + 48 = 144
-                    new OrderItem { Quantity = 4, SKU = "C", Price = 20 },
-                    new OrderItem { Quantity = 2, SKU = "D", Price = 15 } //COMBINE 70 - 20% = 56
-                                                                          //SINGLE = 50 + 50 + 30 + 20 + 20
-                }
+                    new OrderItem { Quantity = 5, SKU = "A", Price = 50 }, // 130 
+                    new OrderItem { Quantity = 5, SKU = "B", Price = 30 }, // 45 + 45 = 90
+                    new OrderItem { Quantity = 1, SKU = "C", Price = 20 }
+                }                                                          //Items Without Promotion 50 + 50 + 30 + 20 = 150
             };
 
             var promotions = new List<Promotion> {
                 new Promotion { BundleType = BundleType.Multiple, SKU = "A", Quantity = 3, DiscountType = DiscountType.FixedPrice, FixedPriceDiscount = 130},
-                new Promotion { BundleType = BundleType.Multiple, SKU = "B", Quantity = 2 , DiscountType = DiscountType.Percentage, PercentageDiscount = 20},
-                new Promotion { BundleType = BundleType.Combination, SKUs = new List<string> { "C", "D" }, DiscountType = DiscountType.Percentage, PercentageDiscount = 20 }
+                new Promotion { BundleType = BundleType.Multiple, SKU = "B", Quantity = 2 , DiscountType = DiscountType.FixedPrice, FixedPriceDiscount = 45},
+                new Promotion { BundleType = BundleType.Combination, SKUs = new List<string> { "C", "D" }, DiscountType = DiscountType.FixedPrice, FixedPriceDiscount = 30 }
             };
 
             //Act
@@ -75,11 +73,11 @@ namespace PromotionEngineTests
                 orderResults.MultipleBundleItems.Sum(x => x.Amount) +
                 orderResults.CombinationBundleItems.Sum(x => x.Amount);
 
-            Assert.Equal(5, orderResults.SingleItems.Sum(x => x.ItemCount));
-            Assert.Equal(274, orderResults.MultipleBundleItems.Sum(x => x.Amount));
-            Assert.Equal(56, orderResults.CombinationBundleItems.Sum(x => x.Amount));
-            Assert.Equal(170, orderResults.SingleItems.Sum(x => x.TotalPrice));
-            Assert.Equal(500, totalSum);
+            Assert.Equal(4, orderResults.SingleItems.Sum(x => x.ItemCount));
+            Assert.Equal(220, orderResults.MultipleBundleItems.Sum(x => x.Amount));
+            Assert.Equal(0, orderResults.CombinationBundleItems.Sum(x => x.Amount));
+            Assert.Equal(150, orderResults.SingleItems.Sum(x => x.TotalPrice));
+            Assert.Equal(370, totalSum);
         }
 
         [Fact]
@@ -90,16 +88,17 @@ namespace PromotionEngineTests
             {
                 Items = new List<OrderItem>
                 {
-                    new OrderItem { Quantity = 5, SKU = "A", Price = 50 },// 120
-                    new OrderItem { Quantity = 5, SKU = "B", Price = 30 },// 42 + 42 
-                    new OrderItem { Quantity = 1, SKU = "C", Price = 20 } // 0
-                                                                          // 50 + 50 + 30 + 20
+                    new OrderItem { Quantity = 3, SKU = "A", Price = 50 },// 130
+                    new OrderItem { Quantity = 5, SKU = "B", Price = 30 },// 45 + 45
+                    new OrderItem { Quantity = 1, SKU = "C", Price = 20 },
+                    new OrderItem { Quantity = 1, SKU = "D", Price = 15 } // 30
+                                                                          //Items Without Promotion 30
                 }
             };
 
             var promotions = new List<Promotion> {
-                new Promotion { BundleType = BundleType.Multiple, SKU = "A", Quantity = 3, DiscountType = DiscountType.Percentage, PercentageDiscount= 20},
-                new Promotion { BundleType = BundleType.Multiple, SKU = "B", Quantity = 2 , DiscountType = DiscountType.Percentage, PercentageDiscount = 30},
+                new Promotion { BundleType = BundleType.Multiple, SKU = "A", Quantity = 3, DiscountType = DiscountType.FixedPrice, FixedPriceDiscount = 130},
+                new Promotion { BundleType = BundleType.Multiple, SKU = "B", Quantity = 2 , DiscountType = DiscountType.FixedPrice, FixedPriceDiscount = 45},
                 new Promotion { BundleType = BundleType.Combination, SKUs = new List<string> { "C", "D" }, DiscountType = DiscountType.FixedPrice, FixedPriceDiscount = 30 }
             };
 
@@ -113,13 +112,15 @@ namespace PromotionEngineTests
                 orderResults.MultipleBundleItems.Sum(x => x.Amount) +
                 orderResults.CombinationBundleItems.Sum(x => x.Amount);
 
-            Assert.Equal(4, orderResults.SingleItems.Sum(x => x.ItemCount));
-            Assert.Equal(204, orderResults.MultipleBundleItems.Sum(x => x.Amount));
-            Assert.Equal(0, orderResults.CombinationBundleItems.Sum(x => x.Amount));
-            Assert.Equal(150, orderResults.SingleItems.Sum(x => x.TotalPrice));
-            Assert.Equal(354, totalSum);
+            Assert.Equal(1, orderResults.SingleItems.Sum(x => x.ItemCount));
+            Assert.Equal(220, orderResults.MultipleBundleItems.Sum(x => x.Amount));
+            Assert.Equal(30, orderResults.CombinationBundleItems.Sum(x => x.Amount));
+            Assert.Equal(30, orderResults.SingleItems.Sum(x => x.TotalPrice));
+            Assert.Equal(280, totalSum);
         }
 
+        #region My tests
+        
         [Fact]
         public void ScenarioD()
         {
@@ -158,5 +159,7 @@ namespace PromotionEngineTests
             //Assert.Equal(150, orderResults.SingleItems.Sum(x => x.TotalPrice));
             Assert.Equal(416, totalSum);
         }
+
+        #endregion
     }
 }
